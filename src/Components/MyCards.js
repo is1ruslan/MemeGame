ce38a9329e4cccb41937f34419799d108de191c1
Ruslan <mnogoludey2@gmail.com>
2023-12-27 19:17:43 +0400

    made the mymemes component

--- src/Components/MyCards.js
+++ src/Components/MyCards.js
@@ -1,42 +1,61 @@
 import React, { useState, useEffect } from 'react'
 
 export default function MyCards () {
-    const [memeUrl, setMemeUrl] = useState([])
-
-    // const fetchMeme = async () => {
-    //     const response = await fetch('https://api.memegen.link/templates/')
-    //     const data = await response.json()
-    //     const rand = Math.round(Math.random() * data.length)
-    //     setMemeUrl(data[rand].blank)
-    // }
-
-    let arr = []
-    let myMemes = memeUrl.map((meme, ind) => (
-        <li key={ind}>
-            {<img className='rounded mymemes' src={meme} alt='Random meme'/>}
-        </li>
-    ))
-
-    const fetchMeme = async () => {
-        const response = await fetch('https://api.imgflip.com/get_memes')
-        const data = await response.json()
-        for (let i = 0; i < 7; i++) {
-            const rand = Math.round(Math.random() * data.data.memes.length)
-            arr[i] = data.data.memes[rand].url
-        }
-        setMemeUrl(arr)
-    }
+    const [myMemes, setMyMemes] = useState([])
+    const [unUsedMemes, setUnUsedMemes] = useState()
 
     useEffect(() => {
-        fetchMeme()
+        startMemes()
+        console.log('11')
     }, [])
+
+    async function startMemes() {
+        let arr = []
+        const response = await fetch('https://api.imgflip.com/get_memes')
+        const data = await response.json()
+        const memes = data.data.memes
+        console.log(memes.length)
+
+        for (let i = 0; i < 7; i++) {
+            const rand = Math.round(Math.random() * memes.length)
+            arr[i] = memes[rand].url
+        }
+        setMyMemes(arr)
+        setUnUsedMemes(memes)
+    }
+
+    // const fetchMeme = async () => {
+    //     const response = await fetch('https://api.imgflip.com/get_memes')
+    //     const data = await response.json()
+    //     for (let i = 0; i < 7; i++) {
+    //         const rand = Math.round(Math.random() * data.data.memes.length)
+    //         arr[i] = data.data.memes[rand].url
+    //     }
+    //     setMemeUrl(arr)
+    // }
+
+    const changeMeme = async (ind) => {
+        const memes = unUsedMemes
+        const rand = Math.round(Math.random() * memes.length)
+        const newMeme = myMemes.map((meme, i) =>
+            i === ind ?
+            memes[rand].url :
+            meme
+        )
+        setMyMemes(newMeme);
+    }
+
+    let Memes = myMemes.map((meme, ind) => (
+        <li key={ind}>
+            {<img className='rounded mymemes' src={meme} onClick={() => changeMeme(ind)} alt='Random meme'/>}
+        </li>
+    ))
 
     return (
         <div>
             <div className="container">
-                <button className="btn btn-warning" onClick={fetchMeme}>Warning!!!</button>
                 <ul>
-                    {myMemes}
+                    {Memes}
                 </ul>
             </div>
         </div>
